6ed916f6c0e6ee6b22f217d8993e465749588a19
nindiseptiani13 <aaherdi832@gmail.com>
2020-05-13 14:52:43 +0700

    tugas bintang tibalik

---
+++ struktur data 1/Bintang.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  for(int i=1;i<6;i++)
+  {
+     for (int j=6;j>=i;j--)
+      {
+       cout<<" ";
+       }
+        for (int k=1;k<=i;k++)
+        cout<<"*";
+       cout<<endl;
+     }
+   }
