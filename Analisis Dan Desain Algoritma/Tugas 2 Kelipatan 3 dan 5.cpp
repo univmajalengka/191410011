bc6a8cc4ff5afe7d9d1addb8844974afbbf071e7
nindi septiani <nindi septiani>
2021-01-13 13:29:33 +0700

    tugas2

---
+++ Analisis Dan Desain Algoritma/Tugas 2 Kelipatan 3 dan 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i = 1; i <= 100; i++){
+      if (i%3==0 && i%5 ==0) {
+        cout<<"upin ipin"<< endl;  
+      }else if (i%3==0){ cout<<"upin"<<endl;    
+	  }else if (i%5==0){ cout<<"ipin"<<endl; 
+    } else { 
+cout<<i<<endl; 
+}
+}
+    return 0;		
+}
